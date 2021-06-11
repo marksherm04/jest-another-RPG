3458466587ffb3da059baac00c6c6205e39576ed
Mark S <marksherm04@gmail.com>
2021-06-11 16:42:13 -0400

    updated 3 tests to add get player health value, check to see if player is alive or not and subtract from player's health, and added functions into player.js

--- __tests__/Potion.test.js
+++ __tests__/Potion.test.js
@@ -1,7 +1,7 @@
 const Potion = require('../lib/Potion');
 
 test('creates a health potion object', () => {
-    const potion = new Potion();
+    const potion = new Potion('health');
 
     expect(potion.name).toBe('health');
     expect(potion.value).toEqual(expect.any(Number));
